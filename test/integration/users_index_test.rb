4e55e8d66cfee2a778b6d07205b8f5ba9ffce635
daglilley <daglilley@gmail.com>
2017-04-20 22:31:23 +0900

    Attempt to write integration test...

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin     = users(:michael)
     @non_admin = users(:archer)
+    # @inactive  = users(:lancer)
   end
 
   test "index as admin including pagination and delete links" do
@@ -28,6 +29,15 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+  
+  test "index with yet unactivated users" do
+    log_in_as(@non_admin)
+    get users_path
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      assert user.activated? == true
+    end
   end
 
 end
