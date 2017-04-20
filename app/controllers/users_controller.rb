96d8c83faebc2d65135beec702f03f0e7405d642
daglilley <daglilley@gmail.com>
2017-04-20 21:38:28 +0900

    Don't show yet unactivated users in user list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
   
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
   end
   
   def new
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    # TODO: Determine of the following line is correct
+    redirect_to root_url and return unless @user.activated?
   end
   
   def new
