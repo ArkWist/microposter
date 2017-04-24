138914805b0ed3f7b9e82b6bad35e994531aff77
daglilley <daglilley@gmail.com>
2017-04-24 22:21:57 +0900

    Fix missing host to error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,8 @@ Rails.application.configure do
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
+  
+  Rails.application.routes.default_url_options[:host] = host
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
